55c2cd0271fa6cbfaffd75e7ca78bc2e0ccaea00
hxezu <hailey_apple@icloud.com>
2025-03-20 14:20:38 +0900

    New Project Added : sticky animation with gsap

---
+++ sticky-contents/script.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.utils.toArray('.con01 .listBox .box').forEach((selector)=>{
+
+        gsap.timeline({
+            scrollTrigger:{
+                trigger : selector,
+                start : '0% 20%',
+                end : '0% 0%',
+                scrub : 1,
+                markers: true
+            }
+        })
+        .to(selector, {transform: 'rotateX(-10deg) scale(0.9)', transformOrigin:'top', filter:'brightness(0.3)'},0)
+    })
+
+    //가로 스크롤이 생겨서 부모요소에 overflow hidden 을 적용해야 하는 경우 유튜브 참고
+}
