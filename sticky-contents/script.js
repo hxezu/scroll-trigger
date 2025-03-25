269b6bb1702e80a2b3174c92fc18d45640d79088
hxezu <hailey_apple@icloud.com>
2025-03-25 14:40:21 +0900

    feat: change image of x-iteration

--- sticky-contents/script.js
+++ sticky-contents/script.js
@@ -9,7 +9,7 @@ window.onload = function(){
                 start : '0% 20%',
                 end : '0% 0%',
                 scrub : 1,
-                markers: true
+                //markers: true
             }
         })
         .to(selector, {transform: 'rotateX(-10deg) scale(0.9)', transformOrigin:'top', filter:'brightness(0.3)'},0)
